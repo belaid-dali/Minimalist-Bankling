2fd7508ff8d3a709f493cc3225df31b3261cde67
belaid-dali <daliomar.belaid@gmail.com>
2024-05-30 12:07:59 -0700

    implementing another feature the request loan

--- script.js
+++ script.js
@@ -159,6 +159,20 @@ btnTransfer.addEventListener('click', function(e) {
   }
 });
 
+btnLoan.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+  if(amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // add loan to movements
+    currentAccount.movements.push(amount);
+
+    updateUI(currentAccount);
+
+  }
+  inputLoanAmount.value = inputLoanAmount.value = '';
+})
+
 btnClose.addEventListener('click', function(e) {
   e.preventDefault();
 
