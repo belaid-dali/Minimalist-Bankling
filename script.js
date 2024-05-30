1de032d0a60766c7a6cc7db8527e50adc3fea1ae
belaid-dali <daliomar.belaid@gmail.com>
2024-05-30 10:13:48 -0700

    implementing login

--- script.js
+++ script.js
@@ -78,25 +78,22 @@ const displayMovements = function(movements) {
 
   });
 }
-displayMovements(account1.movements);
-// console.log(containerMovements.innerHTML);
 
 const calcDisplayBalance = function(movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
   labelBalance.textContent = `Balance: ${balance}€`;
 }
-calcDisplayBalance(account1.movements);
 
-const calcDisplaySummary = function(movements) {
-  const incomes = movements.filter(mov => mov > 0).reduce((acc, mov) => acc + mov, 0);
+const calcDisplaySummary = function(acc) {
+  const incomes = acc.movements.filter(mov => mov > 0).reduce((acc, mov) => acc + mov, 0);
   labelSumIn.textContent = `${incomes}€`;
 
-  const outcomes = movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0);
+  const outcomes = acc.movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0);
   labelSumOut.textContent = `${Math.abs(outcomes)}€`;
 
-  const interest = movements
+  const interest = acc.movements
     .filter(mov => mov > 0)
-    .map(deposit => deposit * 1.2 / 100)
+    .map(deposit => deposit * acc.interestRate / 100)
     .filter((int, i, arr) => {
     console.log(arr);
     return int >= 1;
@@ -104,7 +101,6 @@ const calcDisplaySummary = function(movements) {
   .reduce((acc, int) => acc + int, 0);
   labelSumInterest.textContent = `${interest}€`;
 }
-calcDisplaySummary(account1.movements);
 
 const createUserNmaes = function(accs) {
   // forEach => side effect 
@@ -113,3 +109,29 @@ const createUserNmaes = function(accs) {
   });
 }
 createUserNmaes(accounts);
+
+// event handlers
+let currentAccount;
+btnLogin.addEventListener('click', function(e) {
+  e.preventDefault();
+  
+  currentAccount = accounts.find(acc => acc.username === inputLoginUsername.value);
+  console.log(currentAccount);
+
+  if(currentAccount?.pin === Number(inputLoginPin.value)) {
+    // display UI and welcome message 
+    labelWelcome.textContent = `Welcome back!, ${currentAccount.owner.split(' ')[0]}`;
+    containerApp.style.opacity = 100;
+
+    // clear the iputs fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+
+    // display movements 
+    displayMovements(currentAccount.movements);
+    // display blance
+    calcDisplayBalance(currentAccount.movements);
+    // display summary
+    calcDisplaySummary(currentAccount);
+  }
+})
